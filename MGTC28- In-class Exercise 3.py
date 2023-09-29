b2db7550adc86ef758a70e7b9070ba1cde5adf5d
cnthwng <145368830+cnthwng@users.noreply.github.com>
2023-09-29 12:43:51 -0400

    Lucky_number_generator.py

--- MGTC28- In-class Exercise 3.py
+++ MGTC28- In-class Exercise 3.py
@@ -1,52 +1,23 @@
-#!/usr/bin/env python
-# coding: utf-8
+# Lucky Number Generator
 
-# # Lucky Number Generator
-
-# """
-# This program will generate a list of six different random lucky numbers ranging from 1 to 49, to simulate the weekly draw of numbers for Lotto 649.
-# User will run the program by calling the function defined below in order to generate the six random values. 
-# """
-
-# In[1]:
+"""
+This program will generate a list of six random lucky numbers ranging from 1 to 49, to simulate the weekly draw of numbers for Lotto 649.
+The user will run the program by calling the function defined below in order to generate the six random values. 
+"""
 
 
-import random # Random library imported as its functions are required to generate a list of random numbers.
+import random #Random library imported as its functions are required to generate a list of random numbers.
 
 
-# In[9]:
+
+def six_lucky_numbers(): # Defined a function to return a list of six random numbers.
+    return random.sample(range(1, 50), 6) # The function will return six numbers from numbers 1-49.
 
 
-def six_lucky_numbers(): # Defined a function to return a list of 6 random numbers.
-    return random.sample(range(1, 50), 6) # The function will return 6 numbers from numbers 1-49.
+
+# Example of output when the program is run.
+
+six_lucky_numbers() # To generate the list of 6 numbers, the user will call the function six_lucky_numbers()—no input required.
 
 
-# In[ ]:
-
-
-# Examples of output when program is run.
-
-
-# In[10]:
-
-
-six_lucky_numbers() # To generate the list of 6 numbers, user will call the function six_lucky_numbers(). No input required.
-
-
-# In[11]:
-
-
-six_lucky_numbers()
-
-
-# In[14]:
-
-
-six_lucky_numbers()
-
-
-# In[15]:
-
-
-six_lucky_numbers()
 
