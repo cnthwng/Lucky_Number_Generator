ce4d2a4f4c11f2d98dcadf4aa1eaaaadc809c6a2
cnthwng <145368830+cnthwng@users.noreply.github.com>
2023-09-29 13:26:06 -0400

    Updated generator_with_modifications.py

--- generator_with_modifications.py
+++ generator_with_modifications.py
@@ -1,42 +1,26 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # Lucky Number Generator
+# Lucky Number Generator
 
 # """
 # This function returns a list of random numbers based on a range (x,y) and total numbers to generate (z) specified from the user.
 # The total numbers to generate must be less then, or equal to the range inputted by the user.
 # """
 
-# In[2]:
-
 
 import random
 
-
-# In[3]:
-
-
 def lucky_numbers(x,y,z):
+    # Examples 
+    # lucky_numbers(1,49,6) example of Lotto 649 draw
+    # [37, 9, 26, 12, 42, 24]
+    
+    # lucky_numbers(1,50,8) Example of Lotto Max Combination draw
+    # [17, 50, 34, 38, 16, 8, 49, 45]
+    
     return random.sample(range(x,y+1), z)
 
 
-# In[4]:
+first_num = int(input("Please provide the 1st number of your range: "))
+last_num = int(input("Please provide the last number of your range: "))
+total_num = int(input("Please provide the amount of numbers you'd like: "))
 
-
-# Running test cases of the lucky_numbers function created above.
-
-
-# In[5]:
-
-
-# lucky_numbers(1,49,6) Example of Lotto 649 draw
-# >>> [37, 9, 26, 12, 42, 24]
-
-
-# In[6]:
-
-
-# lucky_numbers(1,50,8) Example of Lotto Max Combination draw
-# >>> [17, 50, 34, 38, 16, 8, 49, 45]
-
+print(first_num, last_num, total_num))
